037d66e080c7d4873178ee37a7913ef821e7b4ec
MohammadSobhanSt <habibdokhtmohammadsobhan@gmail.com>
2024-09-05 04:15:24 +0330

    Initial commit

---
+++ accounts/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+def accounts(request):
+    return render(request, "accounts.html")
+
+def login(request):
+    pass
+
+def logout(request):
+    pass
+
+def register(request):
+    pass
